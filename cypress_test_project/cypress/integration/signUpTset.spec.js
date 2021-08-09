4525d3fa4a52cc49db9d2ef6c6af508fbf9a712b
Danylo25 <pasichnykdanylo@gmail.com>
2021-08-09 16:32:27 +0300

    Advanced

---
+++ cypress_test_project/cypress/integration/signUpTset.spec.js
@@ -0,0 +1,26 @@
+/// <reference types="cypress" />
+
+let user;
+
+describe('Second Test', () => {
+    before(function () {
+        cy.visit('http://localhost:1667/#/');
+        cy.task("freshUser").then((object) => {
+            user = object;
+        });
+    });
+
+    it('Sign up with faker', () => {
+        cy.get(':nth-child(3) > .nav-link').click();
+        cy.get(':nth-child(1) > .form-control').type(user.username);
+        cy.get(':nth-child(2) > .form-control').type(user.email);
+        cy.get(':nth-child(3) > .form-control').type(user.password);
+        cy.get('.btn').click();
+
+        cy.get('.swal-modal').should('contain', 'Your registration was successful!');
+
+        cy.get('.swal-button').click();
+        
+        cy.get(':nth-child(4) > .nav-link').should('contain', user.username);
+    })
+  })
